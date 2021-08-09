f58a8158310cd58dc6c1becf52b791a91508cdde
David Szymkowiak <davidszymkowiak@Davids-MA.local>
2021-08-08 19:47:53 -0700

    queries.sql and schema.sql

---
+++ schema.sql
@@ -0,0 +1,9 @@
+-- Example of creating a table
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
